bef196f66bec98b07b3d637fed42538075a40f27
Maxzett <maxi.marzetti03@gmail.com>
2024-05-03 20:50:58 -0300

    feat: add js validation form

--- js/index.js
+++ js/index.js
@@ -11,3 +11,59 @@ cerrar.addEventListener('click', () => {
 });
 
 
+// FORM VALIDATION
+
+document.getElementById('formulario').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    // Validar nombre
+    const nombre = document.getElementById('nombre').value;
+    const errorNombre = document.getElementById('errorNombre');
+    if (nombre.trim() === '') {
+      errorNombre.textContent = 'Por favor, ingrese su nombre y apellido.';
+    } else {
+      errorNombre.textContent = '';
+    }
+  
+    // Validar email
+    const email = document.getElementById('email').value;
+    const errorEmail = document.getElementById('errorEmail');
+    if (email.trim() === '') {
+      errorEmail.textContent = 'Por favor, ingrese su correo electrónico.';
+    } else {
+      errorEmail.textContent = '';
+    }
+  
+    // Validar foto de perfil
+    const img = document.getElementById('img').value;
+    const errorImg = document.getElementById('errorImg');
+    if (img.trim() === '') {
+      errorImg.textContent = 'Por favor, seleccione una foto de perfil.';
+    } else {
+      errorImg.textContent = '';
+    }
+  
+    // Validar lugar
+    const lugar = document.getElementById('lugar').value;
+    const errorLugar = document.getElementById('errorLugar');
+    if (lugar === '') {
+      errorLugar.textContent = 'Por favor, seleccione un lugar.';
+    } else {
+      errorLugar.textContent = '';
+    }
+  
+    // Validar términos y condiciones
+    const terminos = document.getElementById('terminos').checked;
+    const errorTerminos = document.getElementById('errorTerminos');
+    if (!terminos) {
+      errorTerminos.textContent = 
+      'Debe aceptar los términos y condiciones.';
+    } else {
+      errorTerminos.textContent = '';
+    }
+  
+    // Si todos los campos son válidos, se puede enviar el formulario
+    if (nombre.trim() !== '' && email.trim() !== '' && img.trim() !== '' && lugar !== '' && terminos) {
+      this.submit();
+    }
+  });
